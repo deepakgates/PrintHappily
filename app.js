77f604aebc3d56fd2e755e65854763ac50096ac5
VR Deepak <deepakgates@gmail.com>
2017-01-30 23:58:16 +0530

    80 change

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 80);
 var server = app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + server.address().port);
 });
